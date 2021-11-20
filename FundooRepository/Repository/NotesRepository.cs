9f4d92c0e16cd283de980c7c1bca9192a1f66f74
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-11-20 23:29:17 +0530

    [Samiksha]Add:NotesRepository

---
+++ FundooRepository/Repository/NotesRepository.cs
@@ -0,0 +1,21 @@
+﻿using FundooRepository.Context;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundooRepository.Repository
+{
+    public class NotesRepository
+    {       
+        private readonly UserContext userContext;
+
+        private readonly IConfiguration configuration;
+        public NotesRepository(UserContext userContext, IConfiguration configuration)
+        {
+            this.userContext = userContext;
+            this.configuration = configuration;
+        }
+
+    }
+}
