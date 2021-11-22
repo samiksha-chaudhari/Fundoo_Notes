5a8c152adf318cc60db88c33649497f39cc675a1
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-11-22 23:37:35 +0530

    [Samiksha]Add:migration and update database

---
+++ FundooRepository/Migrations/20211122180601_notes.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FundooRepository.Migrations
+{
+    public partial class notes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    NoteId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Reminder = table.Column<string>(nullable: true),
+                    Colour = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Pin = table.Column<bool>(nullable: false),
+                    Archive = table.Column<bool>(nullable: false),
+                    Trash = table.Column<bool>(nullable: false),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.NoteId);
+                    table.ForeignKey(
+                        name: "FK_Notes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_UserId",
+                table: "Notes",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
