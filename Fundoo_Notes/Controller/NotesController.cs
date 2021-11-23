7536e371cd368c94177fb194e3d9d230176c683c
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-11-23 17:35:39 +0530

    [Samiksha]Add:delete note in controller

--- Fundoo_Notes/Controller/NotesController.cs
+++ Fundoo_Notes/Controller/NotesController.cs
@@ -38,5 +38,27 @@ namespace Fundoo_Notes.Controller
                 return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
             }
         }
+
+        [HttpDelete]
+        [Route("api/deletenote")]
+        public IActionResult DeleteNote(int noteId)
+        {
+            try
+            {
+                string result = this.manager.DeleteNote(noteId);
+                if (result.Equals("Note is Deleted"))
+                {
+                    return this.Ok(new ResponseModel<string>() { Status = true, Message = result });
+                }
+                else
+                {
+                    return this.BadRequest(new ResponseModel<string>() { Status = false, Message = result });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
+            }
+        }
     }
 }
