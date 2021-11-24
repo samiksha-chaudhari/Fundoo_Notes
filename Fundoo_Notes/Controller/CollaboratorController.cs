dace7236539b160bb355f9d4048732399b8c7b49
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-11-24 23:59:59 +0530

    [Samiksha]Add: controller and manager

---
+++ Fundoo_Notes/Controller/CollaboratorController.cs
@@ -0,0 +1,18 @@
+﻿using FundooManager.Manager;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fundoo_Notes.Controller
+{
+    public class CollaboratorController: ControllerBase
+    {
+        private readonly CollaboratorManager manager;
+        public CollaboratorController(CollaboratorManager manager)
+        {
+            this.manager = manager;
+        }
+    }
+}
