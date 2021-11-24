9a3ef5f54eadfb7e195a226e68b8ba395409b60d
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-11-24 23:46:49 +0530

    [Samiksha]Add: collaborator model

---
+++ FundooModel/CollaboratorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FundooModel
+{
+    class CollaboratorModel
+    {
+        [Key]
+        public int CollaboratorId { get; set; }
+                
+        [ForeignKey("NotesModel")]
+        public int NoteId { get; set; }         
+        public virtual NotesModel NotesModel { get; set; }
+
+    }
+}
